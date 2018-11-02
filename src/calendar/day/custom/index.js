3caea4b72678e5c9e764204c43c90794aff80374
SHiNYJ <quokka88@SHiNYJ-iMac27.local>
2018-11-02 22:17:00 +0900

    Personalized for development use

--- src/calendar/day/custom/index.js
+++ src/calendar/day/custom/index.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import {
-  TouchableOpacity,
-  Text,
+    // New
+    View,
+    TouchableOpacity,
+    Text,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -9,81 +11,99 @@ import styleConstructor from './style';
 import {shouldUpdate} from '../../../component-updater';
 
 class Day extends Component {
-  static propTypes = {
-    // TODO: disabled props should be removed
-    state: PropTypes.oneOf(['selected', 'disabled', 'today', '']),
+    static propTypes = {
+        // TODO: disabled props should be removed
+        state: PropTypes.oneOf(['selected', 'disabled', 'today', '']),
+        rangeState: PropTypes.oneOf(['range', 'rangeStart', 'rangeEnd', 'rangeStartEnd', '']),
 
-    // Specify theme properties to override specific styles for calendar parts. Default = {}
-    theme: PropTypes.object,
-    marking: PropTypes.any,
-    onPress: PropTypes.func,
-    date: PropTypes.object
-  };
+        // Specify theme properties to override specific styles for calendar parts. Default = {}
+        theme: PropTypes.object,
+        marking: PropTypes.any,
+        onPress: PropTypes.func,
+        date: PropTypes.object
+    };
 
-  constructor(props) {
-    super(props);
-    this.style = styleConstructor(props.theme);
-    this.onDayPress = this.onDayPress.bind(this);
-    this.onDayLongPress = this.onDayLongPress.bind(this);
-  }
-
-  onDayPress() {
-    this.props.onPress(this.props.date);
-  }
-  onDayLongPress() {
-    this.props.onLongPress(this.props.date);
-  }
-
-  shouldComponentUpdate(nextProps) {
-    return shouldUpdate(this.props, nextProps, ['state', 'children', 'marking', 'onPress', 'onLongPress']);
-  }
-
-  render() {
-    let containerStyle = [this.style.base];
-    let textStyle = [this.style.text];
-
-    let marking = this.props.marking || {};
-    if (marking && marking.constructor === Array && marking.length) {
-      marking = {
-        marking: true
-      };
-    }
-    const isDisabled = typeof marking.disabled !== 'undefined' ? marking.disabled : this.props.state === 'disabled';
-
-    if (marking.selected) {
-      containerStyle.push(this.style.selected);
-    } else if (isDisabled) {
-      textStyle.push(this.style.disabledText);
-    } else if (this.props.state === 'today') {
-      containerStyle.push(this.style.today);
-      textStyle.push(this.style.todayText);
+    constructor(props) {
+        super(props);
+        this.style = styleConstructor(props.theme);
+        this.onDayPress = this.onDayPress.bind(this);
+        this.onDayLongPress = this.onDayLongPress.bind(this);
     }
 
-    if (marking.customStyles && typeof marking.customStyles === 'object') {
-      const styles = marking.customStyles;
-      if (styles.container) {
-        if (styles.container.borderRadius === undefined) {
-          styles.container.borderRadius = 16;
+    onDayPress() {
+        this.props.onPress(this.props.date);
+    }
+
+    onDayLongPress() {
+        this.props.onLongPress(this.props.date);
+    }
+
+    shouldComponentUpdate(nextProps) {
+        return shouldUpdate(this.props, nextProps, ['state', 'rangeState', 'children', 'marking', 'onPress', 'onLongPress']);
+    }
+
+    render() {
+        let baseStyle = [this.style.base];
+        let textStyle = [this.style.text];
+        let containerStyle = [this.style.base];
+
+        let marking = this.props.marking || {};
+        if (marking && marking.constructor === Array && marking.length) {
+            marking = {
+                marking: true
+            };
         }
-        containerStyle.push(styles.container);
-      }
-      if (styles.text) {
-        textStyle.push(styles.text);
-      }
-    }
+        const isDisabled = typeof marking.disabled !== 'undefined' ? marking.disabled : this.props.state === 'disabled';
 
-    return (
-      <TouchableOpacity
-        style={containerStyle}
-        onPress={this.onDayPress}
-        onLongPress={this.onDayLongPress}
-        activeOpacity={marking.activeOpacity}
-        disabled={marking.disableTouchEvent}
-      >
-        <Text allowFontScaling={false} style={textStyle}>{String(this.props.children)}</Text>
-      </TouchableOpacity>
-    );
-  }
+        if (this.props.rangeState === 'range') {
+            baseStyle.push(this.style.range);
+        } else if (this.props.rangeState.includes('range')) {
+            baseStyle.push(this.style[this.props.rangeState]);
+        }
+
+        if (isDisabled) {
+            textStyle.push(this.style.disabledText);
+        }
+
+        if (this.props.state === 'today') {
+            containerStyle.push(this.style.today);
+            textStyle.push(this.style.todayText);
+        }
+
+        if (marking.selected) {
+            containerStyle.push(this.style.selected);
+            textStyle.push(this.style.selectedText);
+        }
+
+        if (marking.customStyles && typeof marking.customStyles === 'object') {
+            const styles = marking.customStyles;
+            if (styles.container) {
+                if (styles.container.borderRadius === undefined) {
+                    styles.container.borderRadius = 16;
+                }
+                containerStyle.push(styles.container);
+            }
+            if (styles.text) {
+                textStyle.push(styles.text);
+            }
+        }
+
+        return (
+            <View style={baseStyle}>
+                <TouchableOpacity
+                    style={containerStyle}
+                    onPress={this.onDayPress}
+                    onLongPress={this.onDayLongPress}
+                    activeOpacity={marking.activeOpacity}
+                    disabled={marking.disableTouchEvent}
+                >
+                    <Text allowFontScaling={false} style={textStyle}>{String(this.props.children)}</Text>
+                </TouchableOpacity>
+
+                {marking.dot && <View style={this.style.dot}/>}
+            </View>
+        );
+    }
 }
 
 export default Day;
--- src/calendar/day/custom/style.js
+++ src/calendar/day/custom/style.js
@@ -3,41 +3,63 @@ import * as defaultStyle from '../../../style';
 
 const STYLESHEET_ID = 'stylesheet.day.single';
 
-export default function styleConstructor(theme={}) {
-  const appStyle = {...defaultStyle, ...theme};
-  return StyleSheet.create({
-    base: {
-      width: 32,
-      height: 32,
-      alignItems: 'center'
-    },
-    text: {
-      marginTop: Platform.OS === 'android' ? 4 : 6,
-      fontSize: appStyle.textDayFontSize,
-      fontFamily: appStyle.textDayFontFamily,
-      fontWeight: '300',
-      color: appStyle.dayTextColor,
-      backgroundColor: 'rgba(255, 255, 255, 0)'
-    },
-    alignedText: {
-      marginTop: Platform.OS === 'android' ? 4 : 6
-    },
-    selected: {
-      backgroundColor: appStyle.selectedDayBackgroundColor,
-      borderRadius: 16
-    },
-    today: {
-      backgroundColor: appStyle.todayBackgroundColor
-    },
-    todayText: {
-      color: appStyle.todayTextColor
-    },
-    selectedText: {
-      color: appStyle.selectedDayTextColor
-    },
-    disabledText: {
-      color: appStyle.textDisabledColor
-    },
-    ...(theme[STYLESHEET_ID] || {})
-  });
+export default function styleConstructor(theme = {}) {
+    const appStyle = {...defaultStyle, ...theme};
+    return StyleSheet.create({
+        base: {
+            width: 32,
+            height: 32,
+            alignItems: 'center'
+        },
+        text: {
+            marginTop: Platform.OS === 'android' ? 4 : 6,
+            fontSize: appStyle.textDayFontSize,
+            fontFamily: appStyle.textDayFontFamily,
+            fontWeight: '300',
+            color: appStyle.dayTextColor,
+            backgroundColor: 'rgba(255, 255, 255, 0)'
+        },
+        alignedText: {
+            marginTop: Platform.OS === 'android' ? 4 : 6
+        },
+        dot: {
+            width: 6,
+            height: 6,
+            position: 'absolute',
+            bottom: 5,
+            backgroundColor: 'white',
+            borderRadius: 3,
+        },
+        range: {
+            backgroundColor: 'rgba(255,255,255, 10)'
+        },
+        rangeStart: {
+            borderTopLeftRadius: 100,
+            borderBottomLeftRadius: 100,
+        },
+        rangeEnd: {
+            borderTopRightRadius: 100,
+            borderBottomRightRadius: 100,
+        },
+        rangeStartEnd: {
+            borderRadius: 100,
+        },
+        selected: {
+            backgroundColor: appStyle.selectedDayBackgroundColor,
+            borderRadius: 16
+        },
+        today: {
+            backgroundColor: appStyle.todayBackgroundColor
+        },
+        todayText: {
+            color: appStyle.todayTextColor
+        },
+        selectedText: {
+            color: appStyle.selectedDayTextColor
+        },
+        disabledText: {
+            color: appStyle.textDisabledColor
+        },
+        ...(theme[STYLESHEET_ID] || {})
+    });
 }
